2002a7eac4a83a52ddbcace41bff1945bf5e4613
Ryoneme2 <worknarair6@gmail.com>
2023-01-15 04:19:22 +0700

    fix type error

--- pages/admin/music_category/editmusic/[restaurantId]/[id].tsx
+++ pages/admin/music_category/editmusic/[restaurantId]/[id].tsx
@@ -1,97 +1,120 @@
-import React from "react";
+import React from 'react';
 
 // components
 
-import CardSettings from "components/Cards/CardEditStore";
-import CardProfile from "components/Cards/CardProfile";
+import CardSettings from 'components/Cards/CardEditStore';
+import CardProfile from 'components/Cards/CardProfile';
 
 // layout for page
 
-import Admin from "layouts/Admin";
-import CardEditMusicCategory from "components/Cards/CardEditMusicCategory";
+import Admin from 'layouts/Admin';
+import CardEditMusicCategory from 'components/Cards/CardEditMusicCategory';
 
-import { getSession } from "next-auth/react";
-import withReactContent from "sweetalert2-react-content";
-import Swal from "sweetalert2";
-import { PrismaClient } from "@prisma/client";
-import { verify } from "jsonwebtoken"
+import { getSession } from 'next-auth/react';
+import withReactContent from 'sweetalert2-react-content';
+import Swal from 'sweetalert2';
+import { PrismaClient } from '@prisma/client';
+import { verify } from 'jsonwebtoken';
+import { Session } from 'next-auth';
 export async function getServerSideProps(context) {
   const prisma = new PrismaClient();
-  const session = await getSession(context)
+  const session = (await getSession(context)) as Session & {
+    tokenUser: string;
+    fname: string;
+    lname: string;
+  };
 
   if (!session) {
-    return { redirect: { destination: '/auth/admin' } }
+    return { redirect: { destination: '/auth/admin' } };
   }
 
   const secretKey: string = process.env.JWT_SECRET;
-  const user = verify(session.tokenUser, secretKey)
+  const user = verify(session.tokenUser, secretKey);
   const adminType = await prisma.aCCOUNT_TYPE.findFirst({
-    where: { NAME: process.env.TYPE_ADMIN_NAME }
-  })
-  await prisma.$disconnect()
+    where: { NAME: process.env.TYPE_ADMIN_NAME },
+  });
+  await prisma.$disconnect();
   const ownerType = await prisma.aCCOUNT_TYPE.findFirst({
-    where: { NAME: process.env.TYPE_OWNER_NAME }
-  })
-  await prisma.$disconnect()
+    where: { NAME: process.env.TYPE_OWNER_NAME },
+  });
+  await prisma.$disconnect();
   // เลือกทุก property
   const res = await prisma.aCCOUNT_PROFILE.findFirst({
     where: {
       ID: parseInt(user.ID),
       OR: [
         { ACCOUNT_TYPE_ID: adminType.ID },
-        { ACCOUNT_TYPE_ID: ownerType.ID }
-      ]
+        { ACCOUNT_TYPE_ID: ownerType.ID },
+      ],
     },
   });
 
-  await prisma.$disconnect()
+  await prisma.$disconnect();
 
-  const dataRole = await JSON.parse(JSON.stringify(res))
+  const dataRole = await JSON.parse(JSON.stringify(res));
 
   if (!dataRole) {
-    return { redirect: { destination: '/' } }
+    return { redirect: { destination: '/' } };
   }
 
-  let result
-  let result2
+  let result;
+  let result2;
   try {
-
     const response = await prisma.rESTAURANT_MUSIC_STYLE.findFirst({
-      where: { RESTAURANT_ID: parseInt(context.params.restaurantId), ID: parseInt(context.params.id) },
+      where: {
+        RESTAURANT_ID: parseInt(context.params.restaurantId),
+        ID: parseInt(context.params.id),
+      },
       include: {
         MUSIC_STYLE: true,
       },
     });
 
+    await prisma.$disconnect();
 
-    await prisma.$disconnect()
-
-    result = JSON.parse(JSON.stringify(response))
+    result = JSON.parse(JSON.stringify(response));
 
     const response2 = await prisma.rESTAURANT_MUSIC_STYLE.findMany({
       where: { RESTAURANT_ID: parseInt(context.params.restaurantId) },
       include: { MUSIC_STYLE: true },
-      orderBy: [{ ID: "asc" }],
+      orderBy: [{ ID: 'asc' }],
     });
-    await prisma.$disconnect()
+    await prisma.$disconnect();
 
-    result2 = JSON.parse(JSON.stringify(response2))
-
+    result2 = JSON.parse(JSON.stringify(response2));
   } catch (error) {
-    const MySwal = withReactContent(Swal)
-    MySwal.fire({ title: "Error", text: error, icon: "error", confirmButtonText: "close" })
+    const MySwal = withReactContent(Swal);
+    MySwal.fire({
+      title: 'Error',
+      text: error,
+      icon: 'error',
+      confirmButtonText: 'close',
+    });
   }
 
-  return { props: { restaurantData: result, musicData: result2, restaurantId: context.params.restaurantId } };
+  return {
+    props: {
+      restaurantData: result,
+      musicData: result2,
+      restaurantId: context.params.restaurantId,
+    },
+  };
 }
 
-export default function musicCategory({ restaurantData, musicData, restaurantId }) {
-
+export default function musicCategory({
+  restaurantData,
+  musicData,
+  restaurantId,
+}) {
   return (
     <>
-      <div className="flex flex-wrap">
-        <div className="w-full">
-          <CardEditMusicCategory restaurantData={restaurantData} musicData={musicData} restaurantId={restaurantId} />
+      <div className='flex flex-wrap'>
+        <div className='w-full'>
+          <CardEditMusicCategory
+            restaurantData={restaurantData}
+            musicData={musicData}
+            restaurantId={restaurantId}
+          />
         </div>
       </div>
     </>
